a4fcd3a8318fcf3e654eb11652667a233bcc617a
Derrick Abucheri <derrickwitness@gmail.com>
2024-02-23 23:22:43 +0300

    Add ITokenService Interface

--- Logicea_Cards/Models/Card.cs
+++ Logicea_Cards/Models/Card.cs
@@ -2,7 +2,7 @@
 {
     public class Card
     {
-        public  int Id { get; set; }
+        public required int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Color { get; set; } // Ensure format validation
